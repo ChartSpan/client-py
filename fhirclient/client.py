9303d40214fdf2e6931d4767b8c88db96bdaf77f
Chris Tavares <christophertavares@chartspan.com>
2023-07-14 11:22:27 -0700

    Initial token auth works
    Created token server auth class
    Plumbed through settings from client

--- fhirclient/client.py
+++ fhirclient/client.py
@@ -19,9 +19,9 @@ logger = logging.getLogger(__name__)
 class FHIRClient(object):
     """ Instances of this class handle authorizing and talking to SMART on FHIR
     servers.
-    
+
     The settings dictionary supports:
-    
+
         - `app_id`*: Your app/client-id, e.g. 'my_web_app'
         - `app_secret`*: Your app/client-secret
         - `api_base`*: The FHIR service to connect to, e.g. 'https://fhir-api-dstu2.smarthealthit.org'
@@ -30,27 +30,27 @@ class FHIRClient(object):
         - `scope`: Space-separated list of scopes to request, if other than default
         - `launch_token`: The launch token
     """
-    
+
     def __init__(self, settings=None, state=None, save_func=lambda x:x):
         self.app_id = None
         self.app_secret = None
         """ The app-id for the app this client is used in. """
-        
+
         self.server = None
         self.scope = scope_default
         self.redirect = None
         """ The redirect-uri that will be used to redirect after authorization. """
-        
+
         self.launch_token = None
         """ The token/id provided at launch, if any. """
-        
+
         self.launch_context = None
         """ Context parameters supplied by the server during launch. """
-        
+
         self.wants_patient = False
         """ If true and launched without patient, will add the correct scope
         to indicate that the server should prompt for a patient after login. """
-        
+
         self.patient_id = None
         self._patient = None
 
@@ -58,22 +58,24 @@ class FHIRClient(object):
         """ If present, is included as part of the request to authenticate
         with a backend system through a client_assertion parameter
         """
-        
+
+        self.settings = settings
+
         if save_func is None:
             raise Exception("Must supply a save_func when initializing the SMART client")
         self._save_func = save_func
-        
+
         # init from state
         if state is not None:
             self.from_state(state)
-        
+
         # init from settings dict
         elif settings is not None:
             if not 'app_id' in settings:
                 raise Exception("Must provide 'app_id' in settings dictionary")
             if not 'api_base' in settings:
                 raise Exception("Must provide 'api_base' in settings dictionary")
-            
+
             self.app_id = settings['app_id']
             self.app_secret = settings.get('app_secret')
             self.redirect = settings.get('redirect_uri')
@@ -81,13 +83,13 @@ class FHIRClient(object):
             self.scope = settings.get('scope', self.scope)
             self.launch_token = settings.get('launch_token')
             self.jwt_token = settings.get('jwt_token', None)
-            self.server = FHIRServer(self, base_uri=settings['api_base'])
+            self.server = FHIRServer(self, base_uri=settings['api_base'], settings=self.settings)
         else:
             raise Exception("Must either supply settings or a state upon client initialization")
-    
-    
+
+
     # MARK: Authorization
-    
+
     @property
     def desired_scope(self):
         """ Ensures `self.scope` is completed with launch scopes, according to
@@ -99,22 +101,22 @@ class FHIRClient(object):
         elif self.patient_id is None and self.wants_patient:
             scope = ' '.join([scope_patientlaunch, scope])
         return scope
-    
+
     @property
     def ready(self):
         """ Returns True if the client is ready to make API calls (e.g. there
         is an access token or this is an open server).
-        
+
         :returns: True if the server can make authenticated calls
         """
         return self.server.ready if self.server is not None else False
-    
+
     def prepare(self):
         """ Returns True if the client is ready to make API calls (e.g. there
         is an access token or this is an open server). In contrast to the
         `ready` property, this method will fetch the server's capability
         statement if it hasn't yet been fetched.
-        
+
         :returns: True if the server can make authenticated calls
         """
         if self.server:
@@ -122,22 +124,22 @@ class FHIRClient(object):
                 return True
             return self.server.prepare()
         return False
-    
+
     @property
     def authorize_url(self):
         """ The URL to use to receive an authorization token.
         """
         return self.server.authorize_uri if self.server is not None else None
-    
+
     def handle_callback(self, url):
         """ You can call this to have the client automatically handle the
         auth callback after the user has logged in.
-        
+
         :param str url: The complete callback URL
         """
         ctx = self.server.handle_callback(url) if self.server is not None else None
         self._handle_launch_context(ctx)
-    
+
     def authorize(self):
         """ Try to authorize with the server. """
         ctx = self.server.authorize() if self.server is not None else None
@@ -145,13 +147,13 @@ class FHIRClient(object):
 
     def reauthorize(self):
         """ Try to reauthorize with the server.
-        
+
         :returns: A bool indicating reauthorization success
         """
         ctx = self.server.reauthorize() if self.server is not None else None
         self._handle_launch_context(ctx)
         return self.launch_context is not None
-    
+
     def _handle_launch_context(self, ctx):
         logger.debug("SMART: Handling launch context: {0}".format(ctx))
         if 'patient' in ctx:
@@ -161,10 +163,10 @@ class FHIRClient(object):
             logger.warning("SMART: Received an id_token, ignoring")
         self.launch_context = ctx
         self.save_state()
-    
-    
+
+
     # MARK: Current Patient
-    
+
     @property
     def patient(self):
         if self._patient is None and self.patient_id is not None and self.ready:
@@ -181,15 +183,15 @@ class FHIRClient(object):
                 logger.warning("SMART: Patient with id {0} not found".format(self.patient_id))
                 self.patient_id = None
             self.save_state()
-        
+
         return self._patient
-    
+
     def human_name(self, human_name_instance):
         """ Formats a `HumanName` instance into a string.
         """
         if human_name_instance is None:
             return 'Unknown'
-        
+
         parts = []
         for n in [human_name_instance.prefix, human_name_instance.given]:
             if n is not None:
@@ -200,19 +202,19 @@ class FHIRClient(object):
             if len(parts) > 0:
                 parts[len(parts)-1] = parts[len(parts)-1]+','
             parts.extend(human_name_instance.suffix)
-        
+
         return ' '.join(parts) if len(parts) > 0 else 'Unnamed'
-    
-    
+
+
     # MARK: State
-    
+
     def reset_patient(self):
         self.launch_token = None
         self.launch_context = None
         self.patient_id = None
         self._patient = None
         self.save_state()
-    
+
     @property
     def state(self):
         return {
@@ -225,8 +227,9 @@ class FHIRClient(object):
             'launch_token': self.launch_token,
             'launch_context': self.launch_context,
             'jwt_token': self.jwt_token,
+            'settings': self.settings
         }
-    
+
     def from_state(self, state):
         assert state
         self.app_id = state.get('app_id') or self.app_id
@@ -236,9 +239,10 @@ class FHIRClient(object):
         self.patient_id = state.get('patient_id') or self.patient_id
         self.launch_token = state.get('launch_token') or self.launch_token
         self.launch_context = state.get('launch_context') or self.launch_context
-        self.server = FHIRServer(self, state=state.get('server'))
+        self.settings = state.get('settings', self.settings)
+        self.server = FHIRServer(self, state=state.get('server'), settings=self.settings)
         self.jwt_token = state.get('jwt_token') or self.jwt_token
-    
+
     def save_state (self):
         self._save_func(self.state)
 
---
+++ fhirclient/token_server_auth.py
@@ -0,0 +1,138 @@
+import boto3
+from botocore.exceptions import ClientError
+import logging
+from datetime import datetime, timezone
+import json
+
+from auth import FHIRAuth
+
+logger = logging.getLogger(__name__)
+
+def snake_to_camel_case(s: str):
+    """
+    ehr params are passed to fhir server constructor using
+    snake_case keys, but the payload on the wire is in camelCase.
+    This function converts one to the other.
+    """
+    first, *rest = s.split('_')
+    return ''.join([first.lower(), *map(str.title, rest)])
+
+
+def remapKeyCase(d: dict):
+    """
+    Given a dictionary d, replace snake_case keys
+    with the equivalent camelCase ones.
+    """
+    return { snake_to_camel_case(k): v for k, v in d.items() }
+
+
+class TokenServerAuth(FHIRAuth):
+    """
+    Rincon token server auth for FHIR servers.
+    """
+    auth_type = 'tokenserver'
+
+    def __init__(self, state=None):
+        self._token_server_arn = None
+        self._token_server_params = None
+        self.access_token = None
+        self.expires_at = None
+
+        super(TokenServerAuth, self).__init__(state=state)
+
+    @property
+    def ready(self):
+        if self.expires_at and self.expires_at < datetime.now(timezone.utc):
+            self.reset()
+        return bool(self.access_token)
+
+    def reset(self):
+        super(TokenServerAuth, self).reset()
+        self.access_token = None
+        self.expires_at = None
+
+    def authorize(self, server):
+        """
+        Make call to token server to get a new token.
+        """
+        client = boto3.client('lambda')
+        logger.debug(f'Token server params = {self._token_server_params}')
+        params = remapKeyCase(self._token_server_params)
+        try:
+            logger.debug(f'FHIR client requesting token for {params["ehrName"]}')
+            response = client.invoke(
+                FunctionName=self._token_server_arn,
+                Payload=json.dumps(params)
+            )
+
+            if 'Error' in response:
+                logger.error(
+                    f'FHIR client: token server request failed: '
+                    f'{response["Error"]["Code"]}: '
+                    f'{response["Error"]["Message"]}')
+                raise Exception(f'Unable to get token from token server')
+
+            results = json.loads(response["Payload"].read())
+            logger.debug(f"Auth server result={results}")
+            if results['statusCode'] != 200:
+                msg = (
+                    f'Request to token server failed: {results["statusCode"]}: '
+                    f'{results["errorDescription"]}')
+                logger.error(msg)
+                raise Exception(msg)
+
+            self.access_token = results['token']
+            self.expires_at = datetime.fromisoformat(results["expiresAt"])
+        except ClientError as err:
+            logger.error(
+                f'Lambda request to token server failed, code '
+                f'{err.response["Error"]["Code"]}'
+            )
+            raise
+
+        return {}
+
+    def reauthorize(self, server):
+        self.authorize(server)
+
+    def _is_expired(self):
+        # If we have an expiration date and it's expired, return true
+        # Otherwise return false
+        return bool(self.expires_at) and self.expires_at < datetime.now(timezone.utc)
+
+    def can_sign_headers(self):
+        # Do we have a token and is it not expired?
+        return bool(self.access_token) and not self._is_expired()
+
+    def signed_headers(self, headers):
+        if not self.can_sign_headers():
+            raise Exception('Cannot sign heards, don\'t have an access token')
+
+        if headers is None:
+            headers = {}
+
+        headers['Authorization'] = f'Bearer {self.access_token}'
+        return headers
+
+    @property
+    def state(self):
+        s = super(TokenServerAuth, self).state
+        if self._token_server_arn is not None:
+            s['token_server_function_name'] = self._token_server_arn
+        if self._token_server_params is not None:
+            s['token_server_params'] = self._token_server_params
+        if self.access_token is not None:
+            s['access_token'] = self.access_token
+
+    def from_state(self, state):
+        """
+        Update instance variables from given state information
+        """
+        super(TokenServerAuth, self).from_state(state)
+
+        self._token_server_arn = state.get('token_server_function_name', self._token_server_arn)
+        self._token_server_params = state.get('token_server_params', self._token_server_params)
+        self.access_token = state.get('access_token', self.access_token)
+
+logger.debug('Registering token server auth type')
+TokenServerAuth.register()
